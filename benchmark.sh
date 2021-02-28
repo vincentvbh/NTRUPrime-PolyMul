d755e16086dd66ad62f3a7465f6c90e2cf12cc78
Erdem ALKIM <erdemalkim@gmail.com>
2021-02-28 16:41:35 +0300

    Latest benchmarks

--- benchmark.sh
+++ benchmark.sh
@@ -8,19 +8,19 @@ echo -e "\t   N = 1536  \t     N = 1530  \t     N = 1620" |tee -a $out
 for i in ntrulpr761  
 do
   sed -n 's/^NTT cycles: /NTT    :    /p' $DIR$i\_gs\_speed.bin.log |tr '\n' '\t' 
-  sed -n 's/^NTT cycles: /|    /p' $DIR$i\_mr1\_speed.bin.log |tr '\n' '\t'
+  sed -n 's/^NTT cycles: /|    /p' $DIR$i\_rader\_speed.bin.log |tr '\n' '\t'
   sed -n 's/^NTT cycles: /|    /p' $DIR$i\_mr\_speed.bin.log
   sed -n 's/^NTT1s cycles: /NTT1s  :    /p' $DIR$i\_gs\_speed.bin.log |tr '\n' '\t'
-  sed -n 's/^NTT1s cycles: /|    /p' $DIR$i\_mr1\_speed.bin.log |tr '\n' '\t'
+  sed -n 's/^NTT1s cycles: /|    /p' $DIR$i\_rader\_speed.bin.log |tr '\n' '\t'
   sed -n 's/^NTT1s cycles: /|    /p' $DIR$i\_mr\_speed.bin.log
   sed -n 's/^Base mul cycles: /Basemul:    /p' $DIR$i\_gs\_speed.bin.log |tr '\n' '\t'
-  sed -n 's/^Base mul cycles: /|    /p' $DIR$i\_mr1\_speed.bin.log |tr '\n' '\t'
+  sed -n 's/^Base mul cycles: /|    /p' $DIR$i\_rader\_speed.bin.log |tr '\n' '\t'
   sed -n 's/^Base mul cycles: /|    /p' $DIR$i\_mr\_speed.bin.log
   sed -n 's/^invNTT cycles: /invNTT :    /p' $DIR$i\_gs\_speed.bin.log |tr '\n' '\t'
-  sed -n 's/^invNTT cycles: /|    /p' $DIR$i\_mr1\_speed.bin.log |tr '\n' '\t'
+  sed -n 's/^invNTT cycles: /|    /p' $DIR$i\_rader\_speed.bin.log |tr '\n' '\t'
   sed -n 's/^invNTT cycles: /|    /p' $DIR$i\_mr\_speed.bin.log
   sed -n 's/^Poly mul cycles: /Polymul:   /p' $DIR$i\_gs\_speed.bin.log |tr '\n' '\t'
-  sed -n 's/^Poly mul cycles: /|   /p' $DIR$i\_mr1\_speed.bin.log |tr '\n' '\t'
+  sed -n 's/^Poly mul cycles: /|   /p' $DIR$i\_rader\_speed.bin.log |tr '\n' '\t'
   sed -n 's/^Poly mul cycles: /|   /p' $DIR$i\_mr\_speed.bin.log
 
 
@@ -35,24 +35,24 @@ for i in ntrulpr761
 do
   echo -e "\t\t    Speed\t\t" 
   sed -n 's/keypair cycles: /G:     /p' $DIR$i\_gs\_speed.bin.log |tr '\n' '\t'
-  sed -n 's/keypair cycles: /|     /p' $DIR$i\_mr1\_speed.bin.log |tr '\n' '\t'
+  sed -n 's/keypair cycles: /|     /p' $DIR$i\_rader\_speed.bin.log |tr '\n' '\t'
   sed -n 's/keypair cycles: /|     /p' $DIR$i\_mr\_speed.bin.log
   sed -n 's/encaps cycles: /E:    /p' $DIR$i\_gs\_speed.bin.log |tr '\n' '\t'
-  sed -n 's/encaps cycles: /|    /p' $DIR$i\_mr1\_speed.bin.log |tr '\n' '\t'
+  sed -n 's/encaps cycles: /|    /p' $DIR$i\_rader\_speed.bin.log |tr '\n' '\t'
   sed -n 's/encaps cycles: /|    /p' $DIR$i\_mr\_speed.bin.log
   sed -n 's/decaps cycles: /D:    /p' $DIR$i\_gs\_speed.bin.log |tr '\n' '\t'
-  sed -n 's/decaps cycles: /|    /p' $DIR$i\_mr1\_speed.bin.log |tr '\n' '\t'
+  sed -n 's/decaps cycles: /|    /p' $DIR$i\_rader\_speed.bin.log |tr '\n' '\t'
   sed -n 's/decaps cycles: /|    /p' $DIR$i\_mr\_speed.bin.log
 
   echo -e "\t\t    Memory\t\t" 
   sed -n 's/key gen stack usage: /G:      /p' $DIR$i\_gs\_stack.bin.log |tr '\n' '\t'
-  sed -n 's/key gen stack usage: /|      /p' $DIR$i\_mr1\_stack.bin.log |tr '\n' '\t'
+  sed -n 's/key gen stack usage: /|      /p' $DIR$i\_rader\_stack.bin.log |tr '\n' '\t'
   sed -n 's/key gen stack usage: /|      /p' $DIR$i\_mr\_stack.bin.log
   sed -n 's/encaps stack usage: /E:      /p' $DIR$i\_gs\_stack.bin.log |tr '\n' '\t'
-  sed -n 's/encaps stack usage: /|      /p' $DIR$i\_mr1\_stack.bin.log |tr '\n' '\t'
+  sed -n 's/encaps stack usage: /|      /p' $DIR$i\_rader\_stack.bin.log |tr '\n' '\t'
   sed -n 's/encaps stack usage: /|      /p' $DIR$i\_mr\_stack.bin.log
   sed -n 's/decaps stack usage: /D:      /p' $DIR$i\_gs\_stack.bin.log |tr '\n' '\t'
-  sed -n 's/decaps stack usage: /|      /p' $DIR$i\_mr1\_stack.bin.log |tr '\n' '\t'
+  sed -n 's/decaps stack usage: /|      /p' $DIR$i\_rader\_stack.bin.log |tr '\n' '\t'
   sed -n 's/decaps stack usage: /|      /p' $DIR$i\_mr\_stack.bin.log
 
 
@@ -63,24 +63,24 @@ for i in sntrup761
 do
   echo -e "\t\t    Speed\t\t" 
   sed -n 's/keypair cycles: /G:   /p' $DIR$i\_gs\_speed.bin.log |tr '\n' '\t'
-  sed -n 's/keypair cycles: /|   /p' $DIR$i\_mr1\_speed.bin.log |tr '\n' '\t'
+  sed -n 's/keypair cycles: /|   /p' $DIR$i\_rader\_speed.bin.log |tr '\n' '\t'
   sed -n 's/keypair cycles: /|   /p' $DIR$i\_mr\_speed.bin.log
   sed -n 's/encaps cycles: /E:     /p' $DIR$i\_gs\_speed.bin.log |tr '\n' '\t'
-  sed -n 's/encaps cycles: /|     /p' $DIR$i\_mr1\_speed.bin.log |tr '\n' '\t'
+  sed -n 's/encaps cycles: /|     /p' $DIR$i\_rader\_speed.bin.log |tr '\n' '\t'
   sed -n 's/encaps cycles: /|     /p' $DIR$i\_mr\_speed.bin.log
   sed -n 's/decaps cycles: /D:     /p' $DIR$i\_gs\_speed.bin.log |tr '\n' '\t'
-  sed -n 's/decaps cycles: /|     /p' $DIR$i\_mr1\_speed.bin.log |tr '\n' '\t'
+  sed -n 's/decaps cycles: /|     /p' $DIR$i\_rader\_speed.bin.log |tr '\n' '\t'
   sed -n 's/decaps cycles: /|     /p' $DIR$i\_mr\_speed.bin.log
 
   echo -e "\t\t    Memory\t\t" 
   sed -n 's/key gen stack usage: /G:      /p' $DIR$i\_gs\_stack.bin.log |tr '\n' '\t'
-  sed -n 's/key gen stack usage: /|      /p' $DIR$i\_mr1\_stack.bin.log |tr '\n' '\t'
+  sed -n 's/key gen stack usage: /|      /p' $DIR$i\_rader\_stack.bin.log |tr '\n' '\t'
   sed -n 's/key gen stack usage: /|      /p' $DIR$i\_mr\_stack.bin.log
   sed -n 's/encaps stack usage: /E:      /p' $DIR$i\_gs\_stack.bin.log |tr '\n' '\t'
-  sed -n 's/encaps stack usage: /|      /p' $DIR$i\_mr1\_stack.bin.log |tr '\n' '\t'
+  sed -n 's/encaps stack usage: /|      /p' $DIR$i\_rader\_stack.bin.log |tr '\n' '\t'
   sed -n 's/encaps stack usage: /|      /p' $DIR$i\_mr\_stack.bin.log
   sed -n 's/decaps stack usage: /D:      /p' $DIR$i\_gs\_stack.bin.log |tr '\n' '\t'
-  sed -n 's/decaps stack usage: /|      /p' $DIR$i\_mr1\_stack.bin.log |tr '\n' '\t'
+  sed -n 's/decaps stack usage: /|      /p' $DIR$i\_rader\_stack.bin.log |tr '\n' '\t'
   sed -n 's/decaps stack usage: /|      /p' $DIR$i\_mr\_stack.bin.log
 
 
